aedd6c8acb93f9a1ed9ec293e50284412090339c
dhruv-dsa21 <112553838+dhruv-dsa21@users.noreply.github.com>
2022-12-19 15:35:15 +0530

    Time: 300 ms (66.74%), Space: 12.3 MB (71.18%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-     bool solve(vector<int> &arr,int sum,int n,vector<vector<int>> &dp){
+//      bool solve(vector<int> &arr,int sum,int n,vector<vector<int>> &dp){
        
-       if(sum==0){
-           return true;
-       }
-       if(n<0&&sum>0){
-           return false;
-       }
+//        if(sum==0){
+//            return true;
+//        }
+//        if(n<0&&sum>0){
+//            return false;
+//        }
         
-        if(dp[n][sum]!=-1){
-            return dp[n][sum];
-        }
+//         if(dp[n][sum]!=-1){
+//             return dp[n][sum];
+//         }
         
-        bool take,nottake;
-        nottake = solve(arr,sum,n-1,dp);
-        if(arr[n]<=sum){
-            take = solve(arr,sum-arr[n],n-1,dp);
-        }
+//         bool take,nottake;
+//         nottake = solve(arr,sum,n-1,dp);
+//         if(arr[n]<=sum){
+//             take = solve(arr,sum-arr[n],n-1,dp);
+//         }
             
         
-        return dp[n][sum] = take||nottake;
-    }
+//         return dp[n][sum] = take||nottake;
+//     }
     bool canPartition(vector<int>& nums) {
         int sum = 0;
         int n = nums.size();
@@ -33,30 +33,38 @@ public:
         }
         
         sum = sum/2;
-           int dp[n+1][sum+1];
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=sum;j++){
-                if(i==0&&j==0){
-                    dp[i][j] = true;
-                }
-                if(j==0){
-                    dp[i][j] = true;
-                }
-                if(i==0&&j>0){
-                    dp[i][j] = false;
-                }
-            }
-        }
+           
+        vector<bool> temp(sum+1,false);
+            temp[0] = true;
+           
+        // for(int i=0;i<=n;i++){
+        //     for(int j=0;j<=sum;j++){
+        //         if(i==0&&j==0){
+        //             dp[i][j] = true;
+        //         }
+        //         if(j==0){
+        //             dp[i][j] = true;
+        //         }
+        //         if(i==0&&j>0){
+        //             dp[i][j] = false;
+        //         }
+        //     }
+        // }
         for(int i=1;i<=n;i++){
+            vector<bool> dp(sum+1);
+            dp[0] = true;
             for(int j=1;j<=sum;j++){
-                bool take,notake;
+                bool take =false;
+                bool notake;
+               
                 if(nums[i-1]<=j){
-                    take  = dp[i-1][j-nums[i-1]];
+                    take  = temp[j-nums[i-1]];
                 }
-                notake = dp[i-1][j];
-                dp[i][j] = take||notake;
+                notake = temp[j];
+                dp[j] = (take||notake);
             }
+            temp = dp;
         }
-        return dp[n][sum];
+        return temp[sum];
     }
 };
