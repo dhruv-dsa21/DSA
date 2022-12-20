9009f89311b9846d792bb0e6dab7197463942838
dhruv-dsa21 <112553838+dhruv-dsa21@users.noreply.github.com>
2022-12-20 09:24:23 +0530

    Time: 8 ms (93.17%), Space: 10.5 MB (35.11%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -14,6 +14,10 @@ public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         int n = rooms.size();
         vector<int> vis(n,0);
+        //it's a simple problem of disconnected graph. 
+        //if the graph is connected then we can reach to each room.
+        //if it is a disconnected graph then we can't reach to each room.
+        //so we will keep a count variable if it is greater than 1 then graph           is disconnected
         int count = 0;
         for(int i=0;i<n;i++){
             if(!vis[i]){
