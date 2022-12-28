2fc7e8f0c4833ce776e3f2ecd275bb0ea7b18d5a
dhruv-dsa21 <112553838+dhruv-dsa21@users.noreply.github.com>
2022-12-28 21:25:41 +0530

    Time: 1405 ms (5.01%), Space: 105.1 MB (5.00%) - LeetHub

--- 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.cpp
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.cpp
@@ -1,26 +1,36 @@
-class Solution {
-public:
-    int minStoneSum(vector<int>& piles, int k) {
-        priority_queue<int> pq;
-        for(auto it:piles){
-            pq.push(it);
-        }
-        while(k>0){
-            int temp = pq.top();
+class Solution
+{
+    public:
+        int minStoneSum(vector<int> &piles, int k)
+        {
+            priority_queue<int> pq;
+            for (auto it: piles)
+            {
+                pq.push(it);
+            }
+            int minSum = 0;
+            while (!pq.empty())
+            {
+                if (k > 0)
+                {
+                    int temp = pq.top();
+
+                    pq.pop();
+                    int rem = temp / 2;
+                    temp = temp - rem;
+                    pq.push(temp);
+                    k--;
+                }
+                else
+                {
+                    int temp = pq.top();
+                    cout << temp << endl;
+                    minSum += temp;
+                    pq.pop();
+                }
+            }
             
-            pq.pop();
-            int rem = temp/2;
-            temp = temp-rem;
-            pq.push(temp);
-            k--;
+          
+            return minSum;
         }
-        int minSum = 0;
-        while(!pq.empty()){
-            int temp = pq.top();
-            cout<<temp<<endl;
-            minSum+=temp;
-            pq.pop();
-        }
-        return minSum;
-    }
 };
