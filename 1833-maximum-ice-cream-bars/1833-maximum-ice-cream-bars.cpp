20a17ea9d85a2bdccddf99c958f90f1bac8c8c39
dhruv-dsa21 <112553838+dhruv-dsa21@users.noreply.github.com>
2023-01-06 18:46:43 +0530

    Time: 516 ms (5.70%), Space: 76.4 MB (76.68%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
         
-        long long sum = 0;
-        sum = accumulate(costs.begin(),costs.end(),sum);
-        if(sum<=coins){
-            return costs.size();
-        }
+        // long long sum = 0;
+        // sum = accumulate(costs.begin(),costs.end(),sum);
+        // if(sum<=coins){
+        //     return costs.size();
+        // }
         sort(costs.begin(),costs.end());
         // if(coins<costs[0]){
         //     return 0;
         // }
-        long long ans=  0;
+        int ans=  0;
         for(int i=0;i<costs.size();i++){
             if(costs[i]>coins){
                 break;
