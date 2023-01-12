cc05b4d5ca145fe9628db9026f05228cf8050997
dhruv-dsa21 <112553838+dhruv-dsa21@users.noreply.github.com>
2023-01-12 23:38:46 +0530

    Time: 836 ms (59.26%), Space: 300.7 MB (11.85%) - LeetHub

--- 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
@@ -1,19 +1,28 @@
 class Solution {
 public:
     vector<int> dfs(vector<vector<int>> &graph,string &labels,vector<int> &ans,int node,int prev){
+        //hm hrr node ke liye ek vector tyyar krenge
         vector<int> curr_node(26,0);
-        
+        //uss nod ke liye jo uska label h usko 1 
+        //krr denge bcoz 1 toh hoga na
+        //trick:- jb hme graph quesn. me tree de 
+        //rkha ho toh visited array maintain krne 
+        //ki bajay hm prev variable maintain krr skte h
         curr_node[labels[node]-'a'] = 1;
         for(auto it:graph[node]){
             if(it==prev){
                 continue;
             }
             vector<int> temp(26,0);
+            //hrr child ek vector dega labels ka apne
+            //papa ko bss hme use add krna h parent me 
                 temp = dfs(graph,labels,ans,it,node);
             for(int i=0;i<26;i++){
                 curr_node[i]+=temp[i];
             }
         }
+        // answer vector me daal do values jo nikli hr 
+        // node ke liye
         ans[node] = curr_node[labels[node]-'a'];
         return curr_node;
     }
@@ -23,7 +32,7 @@ public:
             a[x[0]].push_back(x[1]);
             a[x[1]].push_back(x[0]);
         }
-        
+        //hmara answer vector 
         vector<int> ans(n,0);
          dfs(a,labels,ans,0,-1);
         return ans;
