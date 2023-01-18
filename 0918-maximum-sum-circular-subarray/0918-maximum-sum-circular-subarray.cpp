c620376d832beaf8225fddb47f79ed3c5f2c8c25
dhruv-dsa21 <112553838+dhruv-dsa21@users.noreply.github.com>
2023-01-18 09:29:13 +0530

    Time: 79 ms (57.22%), Space: 39.9 MB (77.39%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
@@ -6,7 +6,15 @@ public:
         int maxcurrsum = 0;
         int mincurrsum = 0;
         int minsum = INT_MAX;
+        //New-Concept:-application of kadane's algo
+        //If we want to find the max sum of a circular subarray 
+        //then we need to find minimum subarray sum and maximum 
+        //sub array sum.
         
+        //The final answer will either be conventional max subarraysum
+        //or we can find it by subtracting total sum-min subarray sum 
+        //because the remaining will be the max of subarray in circular fashion
+        //dry-run once to clear doubt.
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
             maxcurrsum = maxcurrsum + nums[i];
