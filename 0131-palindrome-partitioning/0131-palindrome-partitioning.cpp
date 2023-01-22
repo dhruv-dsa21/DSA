ab68a826038ff07cefe3e8bf9850e6c42f0053b0
dhruv-dsa21 <112553838+dhruv-dsa21@users.noreply.github.com>
2023-01-23 00:01:12 +0530

    Time: 228 ms (37.99%), Space: 140.7 MB (17.75%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,37 +1,6 @@
-class Solution {
-public:
-    bool checkPalindrome(string str, int startIndex, int lastIndex){
-        while(startIndex <= lastIndex){
-            if(str[startIndex] != str[lastIndex])
-                return false;
-            startIndex++;
-            lastIndex--;
-        }
-        return true;
-    }
-    void palindromePartition(int index, vector<string>& ds, vector<vector<string>>& output, string str){
-        if(index == str.length()){
-            output.push_back(ds);
-            return;
-        }
-        for(int i = index; i < str.length(); i++){
-            if(checkPalindrome(str, index, i)){
-                ds.push_back(str.substr(index, i - index + 1));
-                palindromePartition(i+1, ds, output, str);
-                ds.pop_back();
-            }
-        }
-    }
-    vector<vector<string>> partition(string s) {
-        vector<vector<string>> output;
-        vector<string> ds;
-        palindromePartition(0, ds, output, s);
-        return output;
-    }
-};
 // class Solution {
 // public:
-//         bool isPalindrome(string str, int startIndex, int lastIndex){
+//     bool checkPalindrome(string str, int startIndex, int lastIndex){
 //         while(startIndex <= lastIndex){
 //             if(str[startIndex] != str[lastIndex])
 //                 return false;
@@ -40,24 +9,55 @@ public:
 //         }
 //         return true;
 //     }
-//     void solve(int idx,int n,string &s,vector<string> temp,vector<vector<string>> &ans){
-//         if(idx==n){
-//             ans.push_back(temp);
+//     void palindromePartition(int index, vector<string>& ds, vector<vector<string>>& output, string str){
+//         if(index == str.length()){
+//             output.push_back(ds);
 //             return;
 //         }
-//         for(int i=idx;i<n;i++){
-//             if(isPalindrome(s,idx,i)){
-//                 temp.push_back(s.substr(idx,i-idx+1));
-//                 solve(i,n,s,temp,ans);
-//                 temp.pop_back();
+//         for(int i = index; i < str.length(); i++){
+//             if(checkPalindrome(str, index, i)){
+//                 ds.push_back(str.substr(index, i - index + 1));
+//                 palindromePartition(i+1, ds, output, str);
+//                 ds.pop_back();
 //             }
 //         }
 //     }
 //     vector<vector<string>> partition(string s) {
-//         int n = s.size();
-//         vector<vector<string>> ans;
-//         vector<string> temp;
-//          solve(0,n,s,temp,ans);
-//         return ans;
+//         vector<vector<string>> output;
+//         vector<string> ds;
+//         palindromePartition(0, ds, output, s);
+//         return output;
 //     }
 // };
+class Solution {
+public:
+        bool isPalindrome(string str, int startIndex, int lastIndex){
+        while(startIndex <= lastIndex){
+            if(str[startIndex] != str[lastIndex])
+                return false;
+            startIndex++;
+            lastIndex--;
+        }
+        return true;
+    }
+    void solve(int idx,int n,string &s,vector<string> temp,vector<vector<string>> &ans){
+        if(idx==n){
+            ans.push_back(temp);
+            return;
+        }
+        for(int i=idx;i<n;i++){
+            if(isPalindrome(s,idx,i)){
+                temp.push_back(s.substr(idx,i-idx+1));
+                solve(i+1,n,s,temp,ans);
+                temp.pop_back();
+            }
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        int n = s.size();
+        vector<vector<string>> ans;
+        vector<string> temp;
+         solve(0,n,s,temp,ans);
+        return ans;
+    }
+};
