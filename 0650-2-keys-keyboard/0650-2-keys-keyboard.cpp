bf785c76c74a9d64eab1ee9732f31ec79df463c0
dhruv-dsa21 <112553838+dhruv-dsa21@users.noreply.github.com>
2023-02-09 08:03:51 +0530

    Time: 44 ms (27.55%), Space: 155.7 MB (13.28%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
-    int solve(int &n,int value,int copy){
+    int solve(int &n,int value,int copy,vector<vector<int>> &dp){
         if(value>n){
             return 1e8;
         }
         if(value==n){
             return 1;
         }
+        if(dp[value][copy]!=-1){
+            return dp[value][copy];
+        }
         int fstep = 0;
         int sstep = 0;
-        fstep = 1+solve(n,value+copy,copy);
-        sstep = 2+solve(n,2*value,value);
+        fstep = 1+solve(n,value+copy,copy,dp);
+        sstep = 2+solve(n,2*value,value,dp);
        
-        return min(fstep,sstep);
+        return dp[value][copy] = min(fstep,sstep);
     }
     int minSteps(int n) {
         if(n==1){
             return 0;
         }
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
         
-        
-        return solve(n,1,1);
+        return solve(n,1,1,dp);
         
     }
 };
