8d91f4f43818b10bb79ce31a1d7e3cd10dc6b2f4
dhruv-dsa21 <112553838+dhruv-dsa21@users.noreply.github.com>
2023-02-13 21:54:11 +0530

    Added solution - LeetHub

---
+++ Nth catalan number - GFG/nth-catalan-number.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <boost/multiprecision/cpp_int.hpp>
+using boost::multiprecision::cpp_int;  // https://www.geeksforgeeks.org/factorial-large-number-using-boost-multiprecision-library/
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+
+class Solution
+{
+    public:
+    //Function to find the nth catalan number.
+    cpp_int findCatalan(int n) 
+    {
+        vector<cpp_int> dp(n+1,0);
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int i = 2;i<=n;i++){
+            for(int j=0;j<i;j++){
+                dp[i]+= dp[j]*dp[i-j-1];
+            }
+        }
+        return dp[n];
+    }
+    
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+    //taking count of testcases
+	int t;
+	cin>>t;
+	while(t--) {
+	    
+	    //taking nth number
+	    int n;
+	    cin>>n;
+	    Solution obj;
+	    //calling function findCatalan function
+	    cout<< obj.findCatalan(n) <<"\n";    
+	}
+	return 0;
+}
+// } Driver Code Ends
