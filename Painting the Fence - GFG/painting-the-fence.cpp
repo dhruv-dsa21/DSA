16a620d51c0f0258427edcf779d05e80627cbc0a
dhruv-dsa21 <112553838+dhruv-dsa21@users.noreply.github.com>
2023-02-15 12:04:55 +0530

    Added solution - LeetHub

--- Painting the Fence - GFG/painting-the-fence.cpp
+++ Painting the Fence - GFG/painting-the-fence.cpp
@@ -7,24 +7,32 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
     public:
-    long long solve(int n,int &k,vector<long long int> &dp){
-        int mod = 1e9 + 7;
-        if(n==1){
-            return k;
-        }
-        if(n==2){
-            return k*k;
-        }
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        long long int diff = ((k-1)*(solve(n-1,k,dp)))%mod;
-        long long int same = ((k-1)*(solve(n-2,k,dp)))%mod;
-        return dp[n] = (diff + same)%mod;
-    }
+    // long long solve(int n,int &k,vector<long long int> &dp){
+        
+    //     if(n==1){
+    //         return k;
+    //     }
+    //     if(n==2){
+    //         return k*k;
+    //     }
+    //     if(dp[n]!=-1){
+    //         return dp[n];
+    //     }
+    //     long long int diff = ((k-1)*(solve(n-1,k,dp)))%mod;
+    //     long long int same = ((k-1)*(solve(n-2,k,dp)))%mod;
+    //     return dp[n] = (diff + same)%mod;
+    // }
     long long countWays(int n, int k){
+        int mod = 1e9 + 7;
         vector<long long int> dp(n+1,-1);
-    return solve(n,k,dp);
+        dp[1] = k;
+        dp[2] = k*k;
+        for(int i=3;i<=n;i++){
+         long long int diff = ((k-1)*(dp[i-1]))%mod;
+        long long int same = ((k-1)*(dp[i-2]))%mod;
+        dp[i] = (diff+same)%mod;
+        }
+    return dp[n];
     }
 };
 
