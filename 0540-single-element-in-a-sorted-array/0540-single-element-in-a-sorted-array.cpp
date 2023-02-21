08ed4e797cf2a29121af2b2ba59c70b01dab4be6
dhruv-dsa21 <112553838+dhruv-dsa21@users.noreply.github.com>
2023-02-21 23:52:12 +0530

    Time: 30 ms (55.07%), Space: 22.3 MB (62.74%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -20,9 +20,11 @@ class Solution
             while (st <= end)
             {
                 int mid = st + (end - st) / 2;
+                //if both adjacent numbers are not equal to the number then it is the single element.
                 if(nums[mid-1]!=nums[mid]&&nums[mid+1]!=nums[mid]){
                     return nums[mid];
                 }
+                // one thing we observe if our mid index is even then it has even no. of elements on its left side and if its left adjacent number is equal then we are sure that our single number will be present on the left side of array from mid index.
                 if (mid % 2 == 0)
                 {
                     if (mid>0&&nums[mid - 1] == nums[mid])
