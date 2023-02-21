0b0cb757f0f783f962fcef16dc9dfe59fc55011b
dhruv-dsa21 <112553838+dhruv-dsa21@users.noreply.github.com>
2023-02-21 15:20:51 +0530

    Added solution - LeetHub

--- Max distance between same elements - GFG/max-distance-between-same-elements.cpp
+++ Max distance between same elements - GFG/max-distance-between-same-elements.cpp
@@ -10,17 +10,16 @@ class Solution{
     {
         unordered_map<int,int> mp;
        
+       int mx = 0;
        for(int i=0;i<n;i++){
            if(mp.find(arr[i])==mp.end()){
                mp[arr[i]] = i;
+           }else{
+               mx = max(mx,i-mp[arr[i]]);
            }
        }
-        int mx = 0;
-        for(int i=0;i<n;i++){
-            if(i!=mp[arr[i]]){
-                mx = max(mx,i-mp[arr[i]]);
-            }
-        }
+        
+       
         return mx;
     }
 };
