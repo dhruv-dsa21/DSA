3b6d7af1529b000d91a78725dbf75debd47577a4
dhruv-dsa21 <112553838+dhruv-dsa21@users.noreply.github.com>
2023-02-23 13:12:23 +0530

    Time: 53 ms (54.19%), Space: 21.7 MB (75.42%) - LeetHub

--- 0554-brick-wall/0554-brick-wall.cpp
+++ 0554-brick-wall/0554-brick-wall.cpp
@@ -4,28 +4,24 @@ class Solution
         int leastBricks(vector<vector < int>> &wall)
         {
             int n = wall.size();
+            int ans = 0;
             unordered_map<int, int> mp;
             for (int i = 0; i < n; i++)
             {
                 int prefix = 0;
-                if (wall[i].size() == 1)
-                {
-                    mp[wall[i][0]] = 0;
-                }
-                else
-                {
+               
                     for (int j = 0; j < wall[i].size() - 1; j++)
                     {
                         prefix += wall[i][j];
                         mp[prefix]++;
                     }
-                }
+                
             }
-            int ans = INT_MAX;
+            
             for (auto it: mp)
             {
-                ans = min(ans, n - it.second);
+                ans = max(ans,it.second);
             }
-            return ans;
+            return n-ans;
         }
 };
