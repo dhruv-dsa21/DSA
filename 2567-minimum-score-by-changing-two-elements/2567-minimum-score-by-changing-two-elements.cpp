6071ec9d1796a6dae55de5e6962719ca5dd5975f
dhruv-dsa21 <112553838+dhruv-dsa21@users.noreply.github.com>
2023-02-25 19:45:41 +0530

    Time: 98 ms (52.33%), Space: 34.1 MB (60.68%) - LeetHub

--- 2567-minimum-score-by-changing-two-elements/2567-minimum-score-by-changing-two-elements.cpp
+++ 2567-minimum-score-by-changing-two-elements/2567-minimum-score-by-changing-two-elements.cpp
@@ -3,8 +3,16 @@ public:
     int minimizeSum(vector<int>& nums) {
         int n = nums.size();
         sort(nums.begin(),nums.end());
+        // First thing first we will make the 2
+        //numbers that we will change equal
+        // That will handle the case of low score
+        
+        //AThere will always be 3 possiblities 
+        //1) If both smallest number changed
        int case1 = nums.back() - nums[2];
+        //2) If both largest number gets changed
         int case2 = nums[nums.size() - 3] - nums[0];
+        //3) If smallest and largest number are changed
         int case3 = nums[nums.size() - 2] - nums[1];
         return min(case1,min(case2,case3));
         
