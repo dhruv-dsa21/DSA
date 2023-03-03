b7b2c1013909326707ed7d6dfd2b33d8230be8a8
dhruv-dsa21 <112553838+dhruv-dsa21@users.noreply.github.com>
2023-03-03 13:19:54 +0530

    Time: 3 ms (99.55%), Space: 18.3 MB (78.36%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -5,6 +5,13 @@ public:
         int count = 0;
        int i=0;
         
+        //How the heck is this an easy question?
+        //So basically we will keep one pointer at
+        //0 and other at 1 and after that we will
+        //keep moving j until we find a different number
+        //and then we will increment i and put j index 
+        //element on it and by this process we will be able to 
+        //seperate unique sorted array with rest of the array.
         for(int j = 1;j<n;j++){
             if(nums[i]!=nums[j]){
                 i++;
