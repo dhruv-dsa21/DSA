11f1e8c4bf7e41ff9e6fbcf4c1bfc5f4c9d722f4
dhruv-dsa21 <112553838+dhruv-dsa21@users.noreply.github.com>
2023-03-03 09:34:01 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Find triplets with zero sum/find-triplets-with-zero-sum.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+#include<stdlib.h>
+#include<iostream>
+using namespace std;
+
+// } Driver Code Ends
+/* You are required to complete the function below
+*  arr[]: input array
+*  n: size of array
+*/
+class Solution{
+  public:
+    //Function to find triplets with zero sum.
+    bool findTriplets(int arr[], int n)
+    { 
+        sort(arr,arr+n);
+        for(int i=0;i<n-2;i++){
+            int j = i+1;
+            int k = n-1;
+            while(j<k){
+                int x = arr[i];
+                int y = arr[j];
+                int z = arr[k];
+                if(x+y+z==0){
+                    return true;
+                }else if(x+y+z<0){
+                    j++;
+                }else if(x+y+z>0){
+                    k--;
+                }
+            }
+            
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+	cin>>t;
+	while(t--){
+    	int n;
+    	cin>>n;
+    	int arr[n]={0};
+    	for(int i=0;i<n;i++)
+    		cin>>arr[i];
+    	Solution obj;
+        if(obj.findTriplets(arr, n))
+            cout<<"1"<<endl;
+        else 
+            cout<<"0"<<endl;
+	}
+    return 0;
+}
+// } Driver Code Ends
