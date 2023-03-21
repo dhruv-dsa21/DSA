da222bad0f394122db3e602aa145430b80fc4461
dhruv-dsa21 <112553838+dhruv-dsa21@users.noreply.github.com>
2023-03-21 22:57:17 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Intersection of two sorted Linked lists/intersection-of-two-sorted-linked-lists.cpp
@@ -0,0 +1,106 @@
+//{ Driver Code Starts
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+Node* inputList(int size)
+{
+    Node *head, *tail;
+    int val;
+    
+    cin>>val;
+    head = tail = new Node(val);
+    
+    while(--size)
+    {
+        cin>>val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+void printList(Node* n)
+{
+    while(n)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+}
+
+Node* findIntersection(Node* head1, Node* head2);
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,m;
+	    cin>> n >> m;
+	    
+	    Node* head1 = inputList(n);
+	    Node* head2 = inputList(m);
+	    
+	    Node* result = findIntersection(head1, head2);
+	    
+	    printList(result);
+	    cout<< endl;
+	}
+	return 0;
+}
+
+// } Driver Code Ends
+
+
+/* The structure of the Linked list Node is as follows:
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+*/
+
+Node* findIntersection(Node* head1, Node* head2)
+{
+    Node *dummy = new Node(0);
+    Node *curr = dummy;
+    Node *temp1 = head1;
+    Node *temp2 = head2;
+    while(temp1&&temp2){
+        if(temp1->data==temp2->data){
+            Node *nex = new Node(temp1->data);
+            curr->next = nex;
+            curr = nex;
+            temp1 = temp1->next;
+            temp2 = temp2->next;
+            
+        }else if(temp1->data<temp2->data){
+            temp1 = temp1->next;
+        }else if(temp1->data>temp2->data){
+            temp2 = temp2->next;
+        }
+    }
+    return dummy->next;
+}
